fd133e0bcb22187e9162b7144b116062492f519e
clemens <clemens@clemens>
2014-10-10 10:37:13 +0200

    initial commit for FTP-Server

---
+++ FTP_Server/src/Server.cpp
@@ -0,0 +1,18 @@
+/*
+ * Server.cpp
+ *
+ *  Created on: 09.10.2014
+ *      Author: clemens
+ */
+
+#include "Server.h"
+
+Server::Server() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Server::~Server() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ FTP_Server/src/Server.h
@@ -0,0 +1,17 @@
+/*
+ * Server.h
+ *
+ *  Created on: 09.10.2014
+ *      Author: clemens
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+class Server {
+public:
+	Server();
+	virtual ~Server();
+};
+
+#endif /* SERVER_H_ */
