3c7d7f59d5bcf714065e0bc3fa3013c6242c0954
regenesis <regenesis@regenesis-VirtualBox>
2014-10-10 11:41:35 +0200

    created Client class

---
+++ FTP_Client/src/Client.h
@@ -0,0 +1,17 @@
+/*
+ * Client.h
+ *
+ *  Created on: 10.10.2014
+ *      Author: regenesis
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+class Client {
+public:
+	Client();
+	virtual ~Client();
+};
+
+#endif /* CLIENT_H_ */
